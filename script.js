c6ded80fd1340ee12f7deb4a63aa7a017de1c9ad
SusanBeno <beno.susan@gmail.com>
2020-08-21 13:53:12 -0400

    updating js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 
+// // Add event listener to generate button
+var generateBtn = document.querySelector("#generate")
+generateBtn.addEventListener("click", writepassword);{
+}
+
 // prompt - length of password
 var chooseLength = prompt("What length would you like your password?  Choose a number between 8 and 128")
 console.log(chooseLength)
 
+// validate length
 if (chooseLength >= 8 && chooseLength <= 128) {
 
   var upperCase = confirm("Click OK if you would you like to use uppercase letters?")
   var lowerCase = confirm("Click OK if you would you like to use lowercase letters?")
-  var useNumberCase = confirm("Click OK if you would you like to use numbers")
-  var useSpecialCase = confirm("Click OK if you would you like to use special characters?")
+  var numberCase = confirm("Click OK if you would you like to use numbers")
+  var specialCase = confirm("Click OK if you would you like to use special characters?")
 
+  // capture selected criteria
   var useUpper = ""
   if (upperCase) {
     useUpper = getRandomUpper()
     console.log(useUpper)
   }
-
 
   var useLower = ""
   if (lowerCase) {
@@ -24,22 +30,19 @@ if (chooseLength >= 8 && chooseLength <= 128) {
   }
 
   var useNumber = ""
-  if (useNumberCase) {
-    useNumberCase = getRandomNumber()
+  if (numberCase) {
+    useNumber = getRandomNumber()
     console.log(useNumber)
   }
 
-
   var useSymbol = ""
-  if (useSpecialCase) {
-    useSpecialCase = getRandomSymbol()
+  if (specialCase) {
+    useSymbol = getRandomSymbol()
     console.log(useSymbol)
   }
 
-
-
-
-  if (upperCase || lowerCase || useNumberCase || useSpecialCase) {
+// write password
+  if (upperCase || lowerCase || numberCase || specialCase) {
 
     var passwordChar = writepassword(useUpper, useLower, useNumber, useSymbol)
 
@@ -49,27 +52,16 @@ if (chooseLength >= 8 && chooseLength <= 128) {
 
   }
 
+  // if no characters selected
   else {
     alert("You much choose a character type to continue!")
   }
 
+  // if length does not meet criteria
 }
 else {
-  // stop script here and refresh?
   alert("password must be at least 8 characters and less than 129 characters.");
-  // define variables
-
 }
-
-
-
-// input validated
-
-// if (upperCase = true|| lowerCase = true || useNumber = true || useSpecial - true) {
-//  alert("You selected a password with: " + )
-// }
-
-// if no variables selected
 
 
 
@@ -79,61 +71,48 @@ function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-
 // lowercase
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
 
 // random number
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-
 // random symbol
 function getRandomSymbol() {
-  var symbols = "!@#$%^&*()_+~`|}{[]\:;?><,./-='";
+  var symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-='";
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
 
+// create random assword
 
-// create password
 function writepassword(useUpper, useLower, useNumber, useSymbol) {
-
-
 
   var password = useUpper + useLower + useNumber + useSymbol
 
-  var remainingpasswordLength = chooseLength - password.length
-  for (let index = 0; index < remainingpasswordLength; index++) {
+  // var remainingpasswordLength = chooseLength - password.length
+  for (let index = 0; password.length < chooseLength; index++) {
 
+    console.log(password)
 
     if (upperCase) {
       password = password + getRandomUpper()
-
     }
-
-
 
     if (lowerCase) {
       password = password + getRandomLower()
-
     }
 
-
-    if (useNumberCase) {
+    if (numberCase) {
       password = password + getRandomNumber()
-
     }
 
-
-
-    if (useSpecialCase) {
+    if (specialCase) {
       password = password + getRandomSymbol()
-
     }
 
   }
@@ -142,9 +121,8 @@ function writepassword(useUpper, useLower, useNumber, useSymbol) {
 }
 
 
-
 // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+// ;
 
 
 
@@ -158,7 +136,6 @@ function writepassword(useUpper, useLower, useNumber, useSymbol) {
 
 // }
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writepassword);
+
 
 
