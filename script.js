b0e8b14a0cefd52e5a135853a78b2b719f73280a
SusanBeno <beno.susan@gmail.com>
2020-08-21 16:52:36 -0400

    updating code

--- script.js
+++ script.js
@@ -1,140 +1,125 @@
 
-// // Add event listener to generate button
+// Add event listener to generate button
 var generateBtn = document.querySelector("#generate")
-generateBtn.addEventListener("click", writepassword);{
-}
+generateBtn.addEventListener("click", generatePassword);
 
-// prompt - length of password
-var chooseLength = prompt("What length would you like your password?  Choose a number between 8 and 128")
-console.log(chooseLength)
+function generatePassword() {
+  // prompt - length of password
+  var chooseLength = prompt("What length would you like your password?  Choose a number between 8 and 128")
+  console.log(chooseLength)
 
-// validate length
-if (chooseLength >= 8 && chooseLength <= 128) {
+  // validate length
+  if (chooseLength >= 8 && chooseLength <= 128) {
 
-  var upperCase = confirm("Click OK if you would you like to use uppercase letters?")
-  var lowerCase = confirm("Click OK if you would you like to use lowercase letters?")
-  var numberCase = confirm("Click OK if you would you like to use numbers")
-  var specialCase = confirm("Click OK if you would you like to use special characters?")
+    var upperCase = confirm("Click OK if you would you like to use uppercase letters?")
+    var lowerCase = confirm("Click OK if you would you like to use lowercase letters?")
+    var numberCase = confirm("Click OK if you would you like to use numbers")
+    var specialCase = confirm("Click OK if you would you like to use special characters?")
 
-  // capture selected criteria
-  var useUpper = ""
-  if (upperCase) {
-    useUpper = getRandomUpper()
-    console.log(useUpper)
-  }
+    // capture selected criteria
+    var useUpper = ""
+    if (upperCase) {
+      useUpper = getRandomUpper()
+      console.log(useUpper)
+    }
 
-  var useLower = ""
-  if (lowerCase) {
-    useLower = getRandomLower()
-    console.log(useLower)
-  }
+    var useLower = ""
+    if (lowerCase) {
+      useLower = getRandomLower()
+      console.log(useLower)
+    }
 
-  var useNumber = ""
-  if (numberCase) {
-    useNumber = getRandomNumber()
-    console.log(useNumber)
-  }
+    var useNumber = ""
+    if (numberCase) {
+      useNumber = getRandomNumber()
+      console.log(useNumber)
+    }
 
-  var useSymbol = ""
-  if (specialCase) {
-    useSymbol = getRandomSymbol()
-    console.log(useSymbol)
-  }
+    var useSymbol = ""
+    if (specialCase) {
+      useSymbol = getRandomSymbol()
+      console.log(useSymbol)
+    }
 
-// write password
-  if (upperCase || lowerCase || numberCase || specialCase) {
+    // create password
+    if (upperCase || lowerCase || numberCase || specialCase) {
 
-    var passwordChar = writepassword(useUpper, useLower, useNumber, useSymbol)
+      var passwordChar = writepassword(useUpper, useLower, useNumber, useSymbol)
 
       var passwordText = document.querySelector("#password");
 
-  passwordText.value = passwordChar;
+      passwordText.value = passwordChar;
 
+    }
+
+    // if no characters selected
+    else {
+      alert("You much choose at least one character type to continue!")
+    }
+
+    // if length does not meet criteria
   }
-
-  // if no characters selected
   else {
-    alert("You much choose a character type to continue!")
+    alert("You much choose a password length at least 8 characters and no more than 128 characters.");
   }
 
-  // if length does not meet criteria
-}
-else {
-  alert("password must be at least 8 characters and less than 129 characters.");
-}
 
 
+  // generate random characters
+  // uppercase
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
 
-// generate random characters
-// uppercase
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+  // lowercase
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
 
-// lowercase
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+  // random number
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
 
-// random number
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-// random symbol
-function getRandomSymbol() {
-  var symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-='";
-  return symbols[Math.floor(Math.random() * symbols.length)]
-}
+  // random symbol
+  function getRandomSymbol() {
+    var symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-='";
+    return symbols[Math.floor(Math.random() * symbols.length)]
+  }
 
 
-// create random assword
+  // create random password
+  function writepassword(useUpper, useLower, useNumber, useSymbol) {
 
-function writepassword(useUpper, useLower, useNumber, useSymbol) {
+    var password = useUpper + useLower + useNumber + useSymbol
 
-  var password = useUpper + useLower + useNumber + useSymbol
+    // var remainingpasswordLength = chooseLength - password.length
+    for (let index = 0; password.length < chooseLength; index++) {
 
-  // var remainingpasswordLength = chooseLength - password.length
-  for (let index = 0; password.length < chooseLength; index++) {
+      console.log(password)
 
-    console.log(password)
+      if (upperCase) {
+        password = password + getRandomUpper()
+      }
 
-    if (upperCase) {
-      password = password + getRandomUpper()
+      if (lowerCase) {
+        password = password + getRandomLower()
+      }
+
+      if (numberCase) {
+        password = password + getRandomNumber()
+      }
+
+      if (specialCase) {
+        password = password + getRandomSymbol()
+      }
+
     }
-
-    if (lowerCase) {
-      password = password + getRandomLower()
-    }
-
-    if (numberCase) {
-      password = password + getRandomNumber()
-    }
-
-    if (specialCase) {
-      password = password + getRandomSymbol()
-    }
+    return password
 
   }
-   return password
-
 }
 
-
-// // Assignment Code
-// ;
-
-
-
-// // Write password to the #password input
-// function writepassword() {
-//   var password = generatepassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-
-// }
 
 
 
